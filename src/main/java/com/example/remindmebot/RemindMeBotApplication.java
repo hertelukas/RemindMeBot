96b457593062b6ea69c4de2c95661405f63470b0
lukas <luki.hertel@gmail.com>
2021-12-22 13:06:54 +0100

    Initial commit

---
+++ src/main/java/com/example/remindmebot/RemindMeBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.remindmebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindMeBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemindMeBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/remindmebot/RemindMeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.remindmebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemindMeBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
